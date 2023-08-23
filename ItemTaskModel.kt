c402d4809cc2f9093967af1e62f9b1b17bb19687
20210699 <20210699@duksung.ac.kr>
2023-08-23 11:03:56 +0900

    DashboardFragment 구현, d-day 구현- 충돌 해결

--- ItemTaskModel.kt
+++ ItemTaskModel.kt
@@ -1,10 +1,5 @@
 package com.example.a2023hackathon
 
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
-import java.util.Locale
-
 data class ItemTaskModel(
     var docId: String? = null,
     var title: String? = null,
@@ -15,5 +10,5 @@ data class ItemTaskModel(
     var professor: String? = null,
     var major: String? = null,
     var sub_code: String? = null,
-    var state: String? = "In progress",
 )
+
--- app/src/main/java/com/example/a2023hackathon/DashboardFragment.kt
+++ app/src/main/java/com/example/a2023hackathon/DashboardFragment.kt
@@ -72,19 +72,22 @@
 
 package com.example.a2023hackathon
 
+import TodoAdapter
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a2023hackathon.databinding.FragmentDashboardBinding
-import com.example.a2023hackathon.databinding.FragmentNotificationsBinding
+import com.google.firebase.firestore.Query
 
 class DashboardFragment : Fragment() {
 
     lateinit var binding: FragmentDashboardBinding
+    private val name = ArrayList<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -93,5 +96,49 @@ class DashboardFragment : Fragment() {
         binding = FragmentDashboardBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        binding = FragmentFourBinding.inflate(inflater, container, false)
+//
+//        //         myCheckPermission(requireActivity() as AppCompatActivity)
+//
+//        binding.mainFab.setOnClickListener {
+//            if(MyApplication.checkAuth()){
+//                val intent = Intent(requireContext(), AddActivity::class.java)
+//                startActivity(intent)
+//            }
+//            else {
+//                Toast.makeText(requireContext(), "인증을 진행해 주세요", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        return binding.root
+//    }
+
+    override fun onStart() {
+        super.onStart()
+        if(MyApplication.checkAuth()){
+            MyApplication.db.collection("tasks")
+                .orderBy("d_date", Query.Direction.DESCENDING)
+                .get()
+                .addOnSuccessListener { result ->
+                    val itemList = mutableListOf<ItemTaskModel>()
+                    for(document in result){
+                        val item = document.toObject(ItemTaskModel::class.java)
+                        item.docId = document.id
+                        itemList.add(item)
+                    }
+                    binding.todoRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+                    binding.todoRecyclerView.adapter = TodoTaskAdapter(requireContext(),itemList)
+                    Log.d("2023Hackathon", "${itemList}")
+                }
+                .addOnFailureListener{
+                    Toast.makeText(requireContext(), "데이터 획득 실패", Toast.LENGTH_SHORT).show()
+                }
+        }
     }
 }
