54e4880759a111f57e087f71839d50c557f3e898
yuchaemin2 <kusuri94@gmail.com>
2023-08-25 06:05:20 +0900

    Final

--- app/src/main/java/com/example/a2023hackathon/DashboardFragment.kt
+++ app/src/main/java/com/example/a2023hackathon/DashboardFragment.kt
@@ -1,3 +1,29 @@
+//package com.example.a2023hackathon
+//
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.TextView
+//import androidx.fragment.app.Fragment
+//import androidx.lifecycle.ViewModelProvider
+//import com.example.a2023hackathon.databinding.FragmentDashboardBinding
+//import com.example.a2023hackathon.databinding.FragmentNotificationsBinding
+//
+//class DashboardFragment : Fragment() {
+//
+//    lateinit var binding: FragmentDashboardBinding
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        binding = FragmentDashboardBinding.inflate(inflater, container, false)
+//
+//        return binding.root
+//    }
+////}
+//
 package com.example.a2023hackathon
 
 import android.os.Bundle
@@ -36,10 +62,29 @@ class DashboardFragment : Fragment() {
                     for(document in result){
                         val item = document.toObject(ItemTaskModel::class.java)
                         item.docId = document.id
+                        Log.d("todo", "todo 받아오는 중")
+                        binding.todoRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+                        binding.todoRecyclerView.adapter = TodoTaskAdapter(requireContext(),itemList)
+//
+//                        if(item.state == "1" ){
+//                            binding.todoRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+//                            binding.todoRecyclerView.adapter = TodoTaskAdapter(requireContext(),itemList)
+//
+//                        }
+//                        else{
+//                            binding.doneRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+//                            binding.doneRecyclerView.adapter = DoneTaskAdapter(requireContext(),itemList)
+//                            Log.d("2023Hackathon", "doneTaskAdapter 구현 ")
+//                            Log.d("2023Hackathon", "${item.state}")
+//                        }
+
                         itemList.add(item)
+
+                        TodoTaskAdapter(requireContext(),itemList).moveItemToLast()
+
+                        TodoTaskAdapter(requireContext(),itemList).notifyDataSetChanged()
                     }
-                    binding.todoRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-                    binding.todoRecyclerView.adapter = TodoTaskAdapter(requireContext(),itemList)
+
                 }
                 .addOnFailureListener{
                     Toast.makeText(requireContext(), "데이터 획득 실패", Toast.LENGTH_SHORT).show()
@@ -47,3 +92,188 @@ class DashboardFragment : Fragment() {
         }
     }
 }
+
+
+//
+//package com.example.a2023hackathon
+//
+//import android.os.Bundle
+//import android.util.Log
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.Toast
+//import androidx.fragment.app.Fragment
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import com.example.a2023hackathon.MyApplication.Companion.auth
+//import com.example.a2023hackathon.databinding.FragmentDashboardBinding
+//import com.example.a2023hackathon.databinding.FragmentDetailTaskBinding
+//import com.google.firebase.firestore.FirebaseFirestore
+//import com.google.firebase.firestore.ListenerRegistration
+//import com.google.firebase.firestore.Query
+//import kotlin.math.log
+//
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
+//
+//class DashboardFragment : Fragment() {
+//
+//    private var param1: String? = null
+//    private var param2: String? = null
+//    lateinit var major: String
+//    lateinit var professor: String
+//    lateinit var subCode: String
+//    lateinit var state: String
+//    private lateinit var itemList: MutableList<ItemTaskModel>
+//    private lateinit var todoTaskAdapter: TodoTaskAdapter
+////    private lateinit var doneTaskAdapter: DoneTaskAdapter
+//
+//
+//    lateinit var binding: FragmentDashboardBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//            state = it.getString("state") ?: "1"
+//        }
+//
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        binding = FragmentDashboardBinding.inflate(inflater, container, false)
+//
+//        itemList = mutableListOf() // itemList 초기화
+//
+//        // arguments에서 state 값을 가져와 초기화
+//        state = arguments?.getString("state") ?: "1"
+//
+//        // 어댑터 초기화
+//        todoTaskAdapter = TodoTaskAdapter(requireContext(), itemList)
+////        doneTaskAdapter = DoneTaskAdapter(requireContext(), itemList)
+//
+//        // RecyclerView에 어댑터 설정
+//        if (state == "1") {
+//            binding.todoRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+//            binding.todoRecyclerView.adapter = todoTaskAdapter
+//        } else {
+//            binding.doneRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+////            binding.doneRecyclerView.adapter = doneTaskAdapter
+//        }
+//        return binding.root
+//    }
+//
+//    // Firestore 리스너
+//    private var firestoreListener: ListenerRegistration? = null
+//
+//    // Firestore 참조
+//    private val db = FirebaseFirestore.getInstance()
+//    private val collectionReference = db.collection("users").document(auth.uid.toString()).collection("mytasks")
+//
+//    override fun onStart() {
+//        super.onStart()
+//        // 주석 처리한 코드를 주석 해제하고 isChecked 변수를 사용하여 어댑터를 선택적으로 설정
+//        if (MyApplication.checkAuth()) {
+//            MyApplication.db.collection("users").document(auth.uid.toString()).collection("mytasks")
+//                .orderBy("d_date", Query.Direction.ASCENDING)
+//                .get()
+//                .addOnSuccessListener { result ->
+//                    val itemList = mutableListOf<ItemTaskModel>()
+//                    for (document in result) {
+//                        val item = document.toObject(ItemTaskModel::class.java)
+//                        item.docId = document.id
+//                        itemList.add(item)
+//                    }
+//
+//                    // state 값에 따라 어댑터 선택
+////                    val adapter = if (state == "1") {
+////                        todoTaskAdapter // Todo 어댑터
+////                    } else {
+////                        todoTaskAdapter
+////                        Log.d("2023Hackathon", "doneTaskAdapter")
+//////                        doneTaskAdapter // Done 어댑터
+////                    }
+//
+//                    // RecyclerView에 어댑터 설정
+//                    if (state == "1") {
+//                        binding.todoRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+//                        binding.todoRecyclerView.adapter = todoTaskAdapter // 선택된 어댑터 설정
+//                        Log.d("2023Hackathon", "if work success")
+//                    } else {
+//                        binding.doneRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+//                        binding.doneRecyclerView.adapter = todoTaskAdapter // 선택된 어댑터 설정
+//                        Log.d("2023Hackathon", "doneTaskAdapter")
+//                    }
+//
+//                    Log.d("2023Hackathon", "${itemList}")
+//                }
+//                .addOnFailureListener {
+//                    Toast.makeText(requireContext(), "데이터 획득 실패", Toast.LENGTH_SHORT).show()
+//                }
+//        }
+//    }
+//
+//    // Firestore 리스너 설정
+//    // Firestore 리스너 설정
+//    private fun setupFirestoreListener() {
+//        firestoreListener = collectionReference.addSnapshotListener { snapshot, e ->
+//            if (e != null) {
+//                // 오류 처리
+//                return@addSnapshotListener
+//            }
+//
+//            if (snapshot != null && !snapshot.isEmpty) {
+//                // Firestore에서 데이터가 변경될 때 호출되는 부분
+//                // 변경된 데이터를 처리하고 RecyclerView를 업데이트
+//                val updatedItems = snapshot.toObjects(ItemTaskModel::class.java)
+//
+//                // "done" 상태와 "In progress" 상태를 분류하여 RecyclerView 어댑터 업데이트
+//                val doneItems = updatedItems.filter { it.state == "0" }
+//                val inProgressItems = updatedItems.filter { it.state == "1" }
+//
+//                // RecyclerView 어댑터의 데이터 목록
+//                val todoItemList: MutableList<ItemTaskModel> = mutableListOf()
+//                val doneItemList: MutableList<ItemTaskModel> = mutableListOf()
+//
+//                // 데이터 분류
+//                todoItemList.clear()
+//                todoItemList.addAll(inProgressItems)
+//
+//                doneItemList.clear()
+//                doneItemList.addAll(doneItems)
+//
+//                // RecyclerView 어댑터 업데이트
+//                if (state == "1") {
+//                    binding.todoRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+//                    binding.todoRecyclerView.adapter = todoTaskAdapter
+//                    todoTaskAdapter.updateData(todoItemList)
+//                } else {
+//                    binding.doneRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+////                    binding.doneRecyclerView.adapter = doneTaskAdapter
+////                    doneTaskAdapter.updateData(doneItemList)
+//                }
+//            }
+//        }
+//    }
+//
+//
+//    // Firestore 리스너 해제
+//    private fun removeFirestoreListener() {
+//        firestoreListener?.remove()
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        setupFirestoreListener()
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        removeFirestoreListener()
+//    }
+//}
--- app/src/main/java/com/example/a2023hackathon/TodoTaskAdapter.kt
+++ app/src/main/java/com/example/a2023hackathon/TodoTaskAdapter.kt
@@ -14,92 +14,137 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a2023hackathon.MyApplication.Companion.auth
-import com.example.a2023hackathon.databinding.ItemTaskBinding
-import com.example.a2023hackathon.databinding.ItemTodoTaskBinding
+import com.example.a2023hackathon.databinding.*
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
 class TodoTaskViewHolder(val binding: ItemTodoTaskBinding): RecyclerView.ViewHolder(binding.root)
+class DoneTaskViewHolder(val binding: ItemDoneTaskBinding): RecyclerView.ViewHolder(binding.root)
 
-class TodoTaskAdapter(val context: Context, val itemList: MutableList<ItemTaskModel>): RecyclerView.Adapter<TodoTaskViewHolder>() {
+class TodoTaskAdapter(val context: Context, val itemList: MutableList<ItemTaskModel>):
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoTaskViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        return TodoTaskViewHolder(ItemTodoTaskBinding.inflate(layoutInflater))
+
+        if (viewType == 0) {
+            return DoneTaskViewHolder(ItemDoneTaskBinding.inflate(layoutInflater))
+        } else {
+            return TodoTaskViewHolder(ItemTodoTaskBinding.inflate(layoutInflater))
+        }
     }
 
-    override fun onBindViewHolder(holder: TodoTaskViewHolder, position: Int) {
-        val data = itemList[position]
+    override fun getItemViewType(position: Int): Int {
+        return if (itemList[position].state == "0") {
+            0  //완료된 할일
+        } else {
+            1 // 진행중인 할일
+        }
+    }
 
-        holder.binding.run {
-            todoTaskTitle.text = data.title
-            todoMajorTitle.text = data.name
 
-            val dDay = calculateDateDifference(data.d_date, toDayStr)
-            val dDayText = if (dDay > 0) {
-                "D-$dDay" // D-day가 0 이상일 경우 "D-n" 형식으로 표시
-            } else if(dDay == 0L){
-                "D-day"
-            }else{
-                "${data.d_date}" // 마감일이 지났을 경우 "마감" 표시
-            }
+    override fun onBindViewHolder(holder:RecyclerView.ViewHolder, position: Int) {
+        val data = itemList.get(position)
 
-            // 텍스트뷰의 텍스트 설정
-            todoTaskDDay.text = dDayText
+        if (holder is TodoTaskViewHolder) {
+            holder.binding.run {
+                todoTaskTitle.text = data.title
+                todoMajorTitle.text = data.name
 
-            // 마감일이 지났을 때 빨간색으로 텍스트 색상 변경
-            if (dDay < 0) {
-                todoTaskDDay.setTextColor(Color.RED) // 빨간색
-            } else {
-                // D-day 표시에 대한 색상 설정 (예: 검은색)
-                todoTaskDDay.setTextColor(Color.BLACK)
-            }
-//
+                val dDay = calculateDateDifference(data.d_date, toDayStr)
+                val dDayText = if (dDay > 0) {
+                    "D-$dDay" // D-day가 0 이상일 경우 "D-n" 형식으로 표시
+                } else if (dDay == 0L) {
+                    "D-day"
+                } else {
+                    "${data.d_date}" // 마감일이 지났을 경우 "마감" 표시
+                }
 
-            val docId = data.docId
+                todoTaskDDay.text = dDayText
 
-            todoTaskCheckbox.setOnClickListener {
+                if (dDay < 0) {
+                    todoTaskDDay.setTextColor(Color.RED) // 빨간색
+                } else {
+                    // D-day 표시에 대한 색상 설정 (예: 검은색)
+                    todoTaskDDay.setTextColor(Color.BLACK)
+                }
+
+                // 텍스트뷰의 텍스트 설정
+                //todoTaskDDay.text = dDayText
+
+                val docId = data.docId
+
+                todoTaskCheckbox.setOnClickListener {
 //                val position = holder.adapterPosition
-                Log.d("2023Hackathon", "docId: ${data.docId}") // 확인용 로그 추가
-                Log.d("2023Hackathon", "docId: ${docId}") // 확인용 로그 추가
+                    Log.d("2023Hackathon", "docId: ${data.docId}") // 확인용 로그 추가
+                    Log.d("2023Hackathon", "docId: ${docId}") // 확인용 로그 추가
 
-                if (data.docId != null) {
-                    if (docId != null) {
-                        data.state = if (todoTaskCheckbox.isChecked) "0" else "1"
+                    if (data.docId != null) {
+                        if (docId != null) {
+                            data.state = "0"
 
-                        MyApplication.db.collection("users")
-                            .document(auth.uid.toString())
-                            .collection("mytasks")
-                            .document(docId.toString())
-                            .update("state", data.state)
-                            .addOnSuccessListener {
-                                Log.d("2023Hackathon", "Firestore 데이터 업데이트 성공")
-                            }
-                            .addOnFailureListener { e ->
-                                Log.w("2023Hackathon", "Firestore 데이터 업데이트 실패", e)
-                            }
-                    } else {
-                        Log.d("2023Hackathon", "docId is null") // 확인용 로그 추가
-                    }
-
-                    //
-                    todoTaskCheckbox.setOnCheckedChangeListener { _, isChecked ->
-                        // isChecked 값에 따라 data.isChecked 업데이트
-                        data.isChecked = isChecked
-
-                        // isChecked 값에 따라 텍스트 스트라이크 스루 적용 또는 제거
-                        if (isChecked) {
-                            todoTaskTitle.paintFlags =
-                                todoTaskTitle.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-                            Log.d("2023Hackerthon","취소선 적용")
-
+                            MyApplication.db.collection("users")
+                                .document(auth.uid.toString())
+                                .collection("mytasks")
+                                .document(docId.toString())
+                                .update("state", data.state)
+                                .addOnSuccessListener {
+                                    Log.d("2023Hackathon", "Firestore 데이터 업데이트 성공")
+                                    refreshDataFromFirestore()
+                                }
+                                .addOnFailureListener { e ->
+                                    Log.w("2023Hackathon", "Firestore 데이터 업데이트 실패", e)
+                                }
                         } else {
-                            todoTaskTitle.paintFlags =
-                                todoTaskTitle.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+                            Log.d("2023Hackathon", "docId is null") // 확인용 로그 추가
                         }
-
                     }
+                    moveItemToLast()
+                }
+            }
+        } else if (holder is DoneTaskViewHolder){
+            holder.binding.run {
+                todoTaskTitle.text = data.title
+                doneMajorTitle.text= data.major
+
+                val dDay = calculateDateDifference(data.d_date, toDayStr)
+                val dDayText = if (dDay > 0) {
+                    "D-$dDay" // D-day가 0 이상일 경우 "D-n" 형식으로 표시
+                } else if (dDay == 0L) {
+                    "D-day"
+                } else {
+                    "${data.d_date}" // 마감일이 지났을 경우 "마감" 표시
+                }
+
+                val docId = data.docId
+
+                doneTaskCheckbox.setOnClickListener {
+//                val position = holder.adapterPosition
+                    Log.d("2023Hackathon", "docId: ${data.docId}") // 확인용 로그 추가
+                    Log.d("2023Hackathon", "docId: ${docId}") // 확인용 로그 추가
+
+                    if (data.docId != null) {
+                        if (docId != null) {
+                            data.state = "1"
+
+                            MyApplication.db.collection("users")
+                                .document(auth.uid.toString())
+                                .collection("mytasks")
+                                .document(docId.toString())
+                                .update("state", data.state)
+                                .addOnSuccessListener {
+                                    Log.d("2023Hackathon", "Firestore 데이터 업데이트 성공")
+                                    refreshDataFromFirestore()
+                                }
+                                .addOnFailureListener { e ->
+                                    Log.w("2023Hackathon", "Firestore 데이터 업데이트 실패", e)
+                                }
+                        } else {
+                            Log.d("2023Hackathon", "docId is null") // 확인용 로그 추가
+                        }
+                    }
+                    moveItemToLast()
                 }
             }
         }
@@ -137,6 +182,37 @@ class TodoTaskAdapter(val context: Context, val itemList: MutableList<ItemTaskMo
 
         // 오류 발생 시 기본값으로 -1 반환
         return -1
+    }
+
+    fun moveItemToLast() {
+        val doneItem = itemList.find {it.state == "0"}
+        doneItem?.let {
+            itemList.remove(it)
+            itemList.add(it)
+            notifyDataSetChanged()
+        }
+    }
+
+    fun refreshDataFromFirestore() {
+        // 파이어베이스에서 데이터를 다시 불러온 후에
+        // itemList를 업데이트하고 RecyclerView를 갱신합니다.
+        MyApplication.db.collection("users")
+            .document(auth.uid.toString())
+            .collection("mytasks")
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                val newItemList = mutableListOf<ItemTaskModel>()
+                for (document in querySnapshot.documents) {
+                    val item = document.toObject(ItemTaskModel::class.java)
+                    item?.let {
+                        newItemList.add(item)
+                    }
+                }
+                moveItemToLast()
+            }
+            .addOnFailureListener { e ->
+                Log.w("2023Hackathon", "Firestore 데이터 불러오기 실패", e)
+            }
     }
 
     fun updateData(newItemList: List<ItemTaskModel>) {
